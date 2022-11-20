f55902acca51f6958ecc973f442fe0cc86031dad
Manuel Prinz <manuel@pinguinkiste.de>
2022-11-20 14:20:58 +0100

    Create example use case interface

---
+++ code/jenplane-backend/jenplane_backend/api/example_usecase.py
@@ -0,0 +1,6 @@
+from typing import Protocol
+
+
+class ExampleUseCase(Protocol):
+    def hello_world(self):
+        pass
--- code/jenplane-backend/jenplane_backend/app.py
+++ code/jenplane-backend/jenplane_backend/app.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from .api.example_usecase import ExampleUseCase
 
 app = FastAPI()
 
 
+class DummyExampleUseCase(ExampleUseCase):
+    def __init__(self):
+        pass
+
+    def hello_world(self):
+        return {"message": "Hello World"}
+
+
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+def root(use_case: ExampleUseCase = Depends(DummyExampleUseCase)):
+    return use_case.hello_world()
