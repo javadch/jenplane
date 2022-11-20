aeb29701b48fd818adb5dfa4cadd7e53f485cf19
Manuel Prinz <manuel@pinguinkiste.de>
2022-11-20 20:38:10 +0100

    Remove empty constructor
    Not needed with current solution.

--- code/jenplane-backend/jenplane_backend/domain/example_service.py
+++ code/jenplane-backend/jenplane_backend/domain/example_service.py
@@ -2,9 +2,5 @@ from jenplane_backend.api.example_usecase import ExampleUseCase
 
 
 class ExampleService(ExampleUseCase):
-    # Empty constructor required by FastAPI
-    def __init__(self):
-        pass
-
     def hello_world(self):
         return {"message": "Hello World"}
