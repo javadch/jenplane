3254e245526f9111f48b9f1a80f96eda097e97cd
Manuel Prinz <manuel.prinz@tib.eu>
2022-11-21 12:23:03 +0100

    Make REST API server startable from PyCharm

--- code/jenplane-backend/jenplane_backend/rest_api.py
+++ code/jenplane-backend/jenplane_backend/rest_api.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Depends
 
 from jenplane_backend.api.example_usecase import ExampleUseCase
@@ -11,3 +12,7 @@ example_use_case = ExampleService
 @app.get("/")
 def root(use_case: ExampleUseCase = Depends(example_use_case)):
     return use_case.hello_world()
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
